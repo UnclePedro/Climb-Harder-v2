1ec9a8df68b26093674a4946c3668ba8675303e7
Uncle Pedro <peterforsyth10@gmail.com>
2024-12-11 18:37:46 +1000

    Correctly store new user data in localstorage

--- src/helpers/userHelper.ts
+++ src/helpers/userHelper.ts
@@ -11,15 +11,20 @@ export const getUser = async (): Promise<User> => {
 
   if (user.id === 0) {
     try {
-      const newUser = await axios.post<User>(
-        "http://localhost:8080/generateUser"
-      );
+      const newUser = await axios.post("http://localhost:8080/generateUser");
 
-      localStorage.setItem("user", JSON.stringify(newUser.data));
       console.log("New user is:", newUser);
-      return newUser.data;
+
+      // Access the newUserData and store only id and apiKey in localStorage
+      const userData = {
+        id: newUser.data.newUserData.id,
+        apiKey: newUser.data.newUserData.apiKey,
+      };
+
+      localStorage.setItem("user", JSON.stringify(userData));
+      return userData;
     } catch (error) {
-      console.error("Failed to create new user");
+      console.error("Failed to create new user", error);
       return emptyUser;
     }
   } else {
