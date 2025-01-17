ee52c419a4120ebee60b42e1b7846d93df5dd489
Uncle Pedro <peterforsyth10@gmail.com>
2025-01-17 12:58:11 +1000

    Adjust for prod

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// https://climb-harder-api.peterforsyth.dev
+// https://api.climb-harder.peterforsyth.dev
 // http://localhost:8080
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://api.climb-harder.peterforsyth.dev";
 axios.defaults.withCredentials = true;
 
 export default axios;
